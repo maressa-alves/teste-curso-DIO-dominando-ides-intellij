25b3d577e2c23b2315815d52cc95e65b55e9794d
Maressa Alves <maressa.msoa@gmail.com>
2022-04-30 10:26:22 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        System.out.println(cachorro);
+    }
+}
